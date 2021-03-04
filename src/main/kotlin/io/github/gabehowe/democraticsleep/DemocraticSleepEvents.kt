9e0febd206a6319fac24041430ce1e93a4714294
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-03-04 02:48:38 -0700

    Complete overhaul

--- src/main/kotlin/io/github/gabehowe/democraticsleep/DemocraticSleepEvents.kt
+++ src/main/kotlin/io/github/gabehowe/democraticsleep/DemocraticSleepEvents.kt
@@ -2,56 +2,82 @@ package io.github.gabehowe.democraticsleep
 
 import org.bukkit.Bukkit
 import org.bukkit.World
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerBedEnterEvent
+import org.bukkit.event.player.PlayerBedLeaveEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.world.TimeSkipEvent
 
 class DemocraticSleepEvents(private val democraticSleep: DemocraticSleep) : Listener {
     @EventHandler
-    fun onJoinEvent(event: PlayerJoinEvent) {
-        democraticSleep.allUUIDs.add(event.player.uniqueId)
-    }
-    @EventHandler
     fun onLeaveEvent(event: PlayerQuitEvent) {
-        democraticSleep.allUUIDs.remove(event.player.uniqueId)
+        democraticSleep.votes.remove(event.player.uniqueId)
+        democraticSleep.stopPlayerSleeping(event.player)
     }
-    @EventHandler
-    fun onSleepEvent(event : PlayerBedEnterEvent) {
-        if( democraticSleep.allUUIDs.size <= 1 ) {
-            return
-        }
-        if(event.player.world.time !in 12543..23998) {
-            return
-        }
-        if(event.player.world.environment == World.Environment.NETHER || event.player.world.environment == World.Environment.THE_END) {
-            return
-        }
-        if(democraticSleep.someoneSlept) {
-            if(democraticSleep.yesUUIDs.contains(event.player.uniqueId) || democraticSleep.noUUIDs.contains(event.player.uniqueId)) {
-                return
-            }
-            democraticSleep.yesUUIDs.add(event.player.uniqueId)
-            democraticSleep.totalVotes.inc()
-            if(democraticSleep.totalVotes >= democraticSleep.allUUIDs.size) {
-                democraticSleep.attemptNightSkip()
-            }
-        }
-        if(!democraticSleep.someoneSlept) {
-            democraticSleep.someoneSlept = true
-            if(democraticSleep.yesUUIDs.contains(event.player.uniqueId) || democraticSleep.noUUIDs.contains(event.player.uniqueId)) {
-                return
-            }
-            democraticSleep.yesUUIDs.add(event.player.uniqueId)
-            democraticSleep.totalVotes.inc()
-            Bukkit.getServer().broadcastMessage("§6${event.player.displayName} is sleeping, do /sn or /sy to vote to skip the night")
-            Bukkit.getServer().scheduler.runTaskLater(democraticSleep, Runnable {
-                if(democraticSleep.someoneSlept) {
-                    democraticSleep.attemptNightSkip()
 
-                }
-            },(democraticSleep.voteTime.toDouble() * 20).toLong())
+    @EventHandler
+    fun onTimeSkipEvent(event: TimeSkipEvent) {
+        if ((event.world as CraftWorld).handle.isNight) {
+            return
         }
+
+        democraticSleep.cancelVote()
+        democraticSleep.sleeping.clear()
+    }
+
+    @EventHandler
+    fun onLeaveBedEvent(event: PlayerBedLeaveEvent) {
+        democraticSleep.stopPlayerSleeping(event.player)
+    }
+
+    @EventHandler
+    fun onSleepEvent(event: PlayerBedEnterEvent) {
+        if (Bukkit.getOnlinePlayers().size <= 1) {
+            return
+        }
+        if (event.bedEnterResult != PlayerBedEnterEvent.BedEnterResult.OK) {
+            return
+        }
+        if (event.player.world.environment == World.Environment.NETHER || event.player.world.environment == World.Environment.THE_END) {
+            return
+        }
+        if ((event.player.world as CraftWorld).handle.isDay) {
+            return
+        }
+
+        democraticSleep.votes.remove(event.player.uniqueId)
+        democraticSleep.sleeping.add(event.player.uniqueId)
+
+        if (democraticSleep.sleeping.size == event.player.world.players.size) {
+            democraticSleep.cancelVote()
+            democraticSleep.sleeping.clear()
+            return
+        }
+
+        if (democraticSleep.successfulVote()) {
+            democraticSleep.attemptSkipNight()
+            return
+        }
+
+        if (democraticSleep.sleeping.size != 1) {
+            return
+        }
+
+        democraticSleep.votes.clear()
+
+        Bukkit.getServer()
+            .broadcastMessage("§6${event.player.displayName}§r§6 is sleeping, do /sn or /sy to vote to skip the night")
+
+        democraticSleep.sleepRunnableId = Bukkit.getServer().scheduler.scheduleSyncDelayedTask(
+            democraticSleep,
+            {
+                if (democraticSleep.sleeping.isNotEmpty()) {
+                    democraticSleep.attemptSkipNight()
+                }
+            }, (democraticSleep.voteTimeout.toDouble() * 20).toLong()
+        )
     }
 }
